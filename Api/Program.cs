a5ece699e6c5184141ff0cb33419e837cdc8064c
joshua mitchell <josh23797@gmail.com>
2025-07-25 19:37:48 +1000

    Move CalculatorApi project to Api folder and rename to Api

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,13 @@
+
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapCarter();
 app.Run();
